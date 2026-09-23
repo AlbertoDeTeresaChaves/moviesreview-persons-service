3a4f0db20c8c1cd6239e97f92d5ceeb6c052ab2c
Alberto de Teresa <alberto.trabajo.es@gmail.com>
2026-09-23 14:47:42 +0000

    Updated OpenAPIDefinition

--- src/main/java/com/moviesreview/person/PersonApplication.java
+++ src/main/java/com/moviesreview/person/PersonApplication.java
@@ -1,9 +1,24 @@
 package com.moviesreview.person;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Persons microservice Rest API Documentation",
+				description = "Persons (Actors, Directors,etc) microservice Rest API Documentation",
+				version = "0.1",
+				contact = @Contact(
+						name = "Alberto de Teresa Chaves",
+						email = "alberto.trabajo.es@gmail.com",
+						url = "https://x.com/Beto_rvss"
+				)
+		)
+)
 @EnableConfigurationProperties
 @SpringBootApplication
 public class PersonApplication {
